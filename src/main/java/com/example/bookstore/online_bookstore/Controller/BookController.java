dfb1e3eca9ba59ebd4a02a5ca968484f4a0ca26d
MohitRathi14 <aaditya375singh2@gmail.com>
2025-07-11 09:00:09 +0530

    Implement image upload functionality in book update process

--- src/main/java/com/example/bookstore/online_bookstore/Controller/BookController.java
+++ src/main/java/com/example/bookstore/online_bookstore/Controller/BookController.java
@@ -1,5 +1,5 @@
 package com.example.bookstore.online_bookstore.Controller;
-
+import com.example.bookstore.online_bookstore.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +8,27 @@ import com.example.bookstore.online_bookstore.Services.BookService;
 import com.example.bookstore.online_bookstore.modle.Book;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
     @Autowired
     private BookService bookService;
+
+    BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     // Show all books
     @GetMapping("/books")
@@ -31,4 +46,34 @@ public class BookController {
         model.addAttribute("selectedCategory", category);
         return "books"; // same view as above, just filtered
     }
+    
+    @PostMapping("/admin/books/update")
+    public String updateBook(@ModelAttribute Book book, @RequestParam("image") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            String uploadDir = "uploads/";
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        Path filePath = uploadPath.resolve(fileName);
+        file.transferTo(filePath.toFile());
+
+        book.setImageName(fileName); // override with new image
+        } 
+        else {
+            // Keep the original image if no new one is uploaded
+            Book existing = bookRepository.findById(book.getId()).orElse(null);
+            if (existing != null) {
+                book.setImageName(existing.getImageName());
+            }
+        }
+
+        bookRepository.save(book);
+        return "redirect:/admin/books";
+    }
+   
+
 }
