62f34928222ae1d7be930875dc78b81366a91283
MohitRathi14 <aaditya375singh2@gmail.com>
2025-07-11 08:59:33 +0530

    Add image upload functionality to book saving process in AdminController

--- src/main/java/com/example/bookstore/online_bookstore/Controller/AdminController.java
+++ src/main/java/com/example/bookstore/online_bookstore/Controller/AdminController.java
@@ -1,9 +1,18 @@
 package com.example.bookstore.online_bookstore.Controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.File;
+import java.util.UUID;
+import com.example.bookstore.online_bookstore.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.bookstore.online_bookstore.Services.BookService;
 import com.example.bookstore.online_bookstore.modle.Book;
 
@@ -11,8 +20,14 @@ import com.example.bookstore.online_bookstore.modle.Book;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final BookRepository bookRepository;
+
     @Autowired
     private BookService bookService;
+
+    AdminController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     // Show book list in admin panel
     @GetMapping("/books")
@@ -29,11 +44,27 @@ public class AdminController {
     }
 
     // Save new or updated book
-    @PostMapping("/books/save")
-    public String saveBook(@ModelAttribute Book book) {
-        bookService.addBook(book);
-        return "redirect:/admin/books";
+   @PostMapping("books/save")
+public String saveBook(@ModelAttribute Book book, @RequestParam("image") MultipartFile file) throws IOException {
+    if (!file.isEmpty()) {
+        String uploadDir = System.getProperty("user.dir") + File.separator + "uploads";
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        Path filePath = uploadPath.resolve(fileName);
+        file.transferTo(filePath.toFile());
+
+        book.setImageName(fileName);
     }
+
+    bookRepository.save(book);
+    return "redirect:/admin/books";
+}
+
 
     // Show form to edit book
     @GetMapping("/books/edit/{id}")
